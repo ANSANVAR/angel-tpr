185f247e5237aadd9dec6d2683cb0786ef625819
ANSANVAR <ansanvar2019@gmail.com>
2021-12-07 02:12:25 -0400

    factura

---
+++ database/migrations/2021_12_07_055934_create_ventas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVentasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id('id_ventas');
+            $table-> integer('id_vendedor');
+            $table-> datetime('fecha_venta');
+            $table-> integer('cantidad');
+            $table-> float('tot_venta');
+            $table-> string('Estado', 1);
+            $table-> timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+}
---
+++ database/migrations/2021_12_07_060814_create_factura_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFacturaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('factura', function (Blueprint $table) {
+            $table->id('id_factura');
+            $table-> integer('id_ventas');
+            $table-> integer('id_producto');
+            $table-> integer('cantidad');
+            $table-> float('tot_venta');
+            $table-> datetime('fecha_venta');
+            $table-> string('Estado', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('factura');
+    }
+}
